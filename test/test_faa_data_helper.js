df337b3a038399bfddcac4319d1ecf38840fb8a6
Emily Chai <echai93@gmail.com>
2017-05-22 15:48:52 +0100

    Create test behaviour of FAADataHelper class

---
+++ test/test_faa_data_helper.js
@@ -0,0 +1,8 @@
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+var FAADataHelper = require('../faa_data_helper');
+
+describe('FAADataHelper', function() {
+  var subject = new FAADataHelper();
+});
