f99b511d72d6461fa414105a3c655ff7f1933abe
Emily Chai <echai93@gmail.com>
2017-05-22 15:54:31 +0100

    Make requestAirportStatus function

--- faa_data_helper.js
+++ faa_data_helper.js
@@ -1,5 +1,11 @@
 'use strict';
+var _ = require('lodash');
+var rp = require('request-promise');
+var ENDPOINT = 'http://services.faa.gov/airport/status/';
 
 function FAADataHelper() { }
+
+FAADataHelper.prototype.requestAirportStatus = function(airportCode) {
+};
 
 module.exports = FAADataHelper;
